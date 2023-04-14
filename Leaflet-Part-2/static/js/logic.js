ab0ecdf055a2edd5f090a45d34badf7bd16b52f7
kiErnguyen <k@arbelo.io>
2023-04-13 19:42:29 -0700

    Updating File Structure

---
+++ Leaflet-Part-2/static/js/logic.js
@@ -0,0 +1,111 @@
+var queryUrl = "https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_week.geojson";
+var tectonicplatesUrl = "https://raw.githubusercontent.com/fraxen/tectonicplates/master/GeoJSON/PB2002_boundaries.json"
+
+d3.json(queryUrl).then(function (data) {
+  console.log(data);
+  createFeatures(data.features);
+});
+
+function chooseColor(depth){
+  if (depth < 10) return "#00FF00";
+  else if (depth < 30) return "greenyellow";
+  else if (depth < 50) return "yellow";
+  else if (depth < 70) return "orange";
+  else if (depth < 90) return "orangered";
+  else return "#FF0000";
+}
+
+function createFeatures(earthquakeData) {
+
+  function onEachFeature(feature, layer) {
+    layer.bindPopup(`<h3>Location: ${feature.properties.place}</h3><hr><p>Date: ${new Date(feature.properties.time)}</p><p>Magnitude: ${feature.properties.mag}</p><p>Depth: ${feature.geometry.coordinates[2]}</p>`);
+  }
+
+  var earthquakes = L.geoJSON(earthquakeData, {
+    onEachFeature: onEachFeature,
+
+    pointToLayer: function(feature, latlng) {
+
+      var markers = {
+        radius: feature.properties.mag * 20000,
+        fillColor: chooseColor(feature.geometry.coordinates[2]),
+        fillOpacity: 0.7,
+        color: "black",
+        weight: 0.5
+      }
+      return L.circle(latlng,markers);
+    }
+  });
+
+  createMap(earthquakes);
+}
+
+function createMap(earthquakes) {
+
+  var satellite = L.tileLayer('https://api.mapbox.com/styles/v1/{style}/tiles/{z}/{x}/{y}?access_token={access_token}', {
+    attribution: "© <a href='https://www.mapbox.com/about/maps/'>Mapbox</a> © <a href='http://www.openstreetmap.org/copyright'>OpenStreetMap</a> <strong><a href='https://www.mapbox.com/map-feedback/' target='_blank'>Improve this map</a></strong>",
+    style:    'mapbox/satellite-v9',
+    access_token: api_key
+  });
+  
+  var grayscale = L.tileLayer('https://api.mapbox.com/styles/v1/{style}/tiles/{z}/{x}/{y}?access_token={access_token}', {
+    attribution: "© <a href='https://www.mapbox.com/about/maps/'>Mapbox</a> © <a href='http://www.openstreetmap.org/copyright'>OpenStreetMap</a> <strong><a href='https://www.mapbox.com/map-feedback/' target='_blank'>Improve this map</a></strong>",
+    style:    'mapbox/light-v11',
+    access_token: api_key
+  });
+
+  var outdoors = L.tileLayer('https://api.mapbox.com/styles/v1/{style}/tiles/{z}/{x}/{y}?access_token={access_token}', {
+    attribution: "© <a href='https://www.mapbox.com/about/maps/'>Mapbox</a> © <a href='http://www.openstreetmap.org/copyright'>OpenStreetMap</a> <strong><a href='https://www.mapbox.com/map-feedback/' target='_blank'>Improve this map</a></strong>",
+    style:    'mapbox/outdoors-v12',
+    access_token: api_key
+  });
+
+  tectonicPlates = new L.layerGroup();
+
+    d3.json(tectonicplatesUrl).then(function (plates) {
+
+        console.log(plates);
+        L.geoJSON(plates, {
+            color: "orange",
+            weight: 2
+        }).addTo(tectonicPlates);
+    });
+
+    var baseMaps = {
+        "Satellite": satellite,
+        "Grayscale": grayscale,
+        "Outdoors": outdoors
+    };
+
+    var overlayMaps = {
+        "Earthquakes": earthquakes,
+        "Tectonic Plates": tectonicPlates
+    };
+    
+  var myMap = L.map("map", {
+    center: [
+      37.09, -95.71
+    ],
+    zoom: 5,
+    layers: [satellite, earthquakes, tectonicPlates]
+  });
+
+  var legend = L.control({position: "bottomright"});
+  legend.onAdd = function() {
+    var div = L.DomUtil.create("div", "info legend"),
+    depth = [-10, 10, 30, 50, 70, 90];
+
+    div.innerHTML += "<h3 style='text-align: center'>Depth</h3>"
+
+    for (var i = 0; i < depth.length; i++) {
+      div.innerHTML +=
+      '<i style="background:' + chooseColor(depth[i] + 1) + '"></i> ' + depth[i] + (depth[i + 1] ? '&ndash;' + depth[i + 1] + '<br>' : '+');
+    }
+    return div;
+  };
+  legend.addTo(myMap)
+
+  L.control.layers(baseMaps, overlayMaps, {
+    collapsed: false
+  }).addTo(myMap);
+};
